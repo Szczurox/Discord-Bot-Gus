b57c7362a37b329bbce90811bbbb46e40c54e584
Szczurox <58273015+Szczurox@users.noreply.github.com>
2022-10-14 19:42:34 +0200

    code refactor
    Changed:
    - code refactoring for money/meth value change functions
    - get_lab_value_change_args (utils/args.rs) (parse value change commands values and return them (or None if there is an error))
    - LabValueChangeArgs (utils/args.rs) (return type for get_lab_value_change_args)

--- src/commands/economy/admin/remove.rs
+++ src/commands/economy/admin/remove.rs
@@ -1,79 +1,40 @@
-use mongodb::bson::doc;
 use serenity::framework::standard::macros::command;
 use serenity::framework::standard::{Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
-use crate::constants::economy::{LabField, Money, Meth};
-use crate::utils::economy::get_lab;
-use crate::utils::errors::{missing_argument, wrong_argument, lab_doesnt_exist};
+use crate::constants::economy::{Money, Meth};
+use crate::utils::args::get_lab_value_change_args;
+use crate::utils::errors::wrong_argument;
 
 // Removes specified amount of meth/money from a user 
 // Usage: remove [money/meth] [@member / ID] [amount]
 #[command]
 #[only_in(guilds)]
-pub async fn remove(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    // Get the guild member for the author
-    let member = msg.guild_id.unwrap().member(ctx, msg.author.id).await?;
+pub async fn remove(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    // Parse arguments
+    let lab_value_change_args_wrapped = get_lab_value_change_args(ctx, msg, args).await;
 
-    if member.permissions(ctx).expect("permissions").contains(Permissions::ADMINISTRATOR) {
-        if args.is_empty() {
-            missing_argument(msg, ctx, String::from("TYPE [meth/money]")).await;
-        } else {
-            let added_item_result = args.single::<String>();
-            if added_item_result.is_err() {
-                wrong_argument(msg, ctx, String::from("TYPE [meth/money]")).await;
-                return Ok(())
-            }
-            let added_item: &str = &added_item_result.unwrap()[..];
-
-            if args.is_empty() {
-                missing_argument(msg, ctx, String::from("MEMBER")).await;
-                return Ok(())
-            }
-
-            // Get user from arguments and throw an error if argument is missing
-            let user_result = args.single::<UserId>();
-            if user_result.is_err() {
-                wrong_argument(msg, ctx, String::from("MEMBER")).await;
-                return Ok(())
-            }
-            let user: UserId = user_result.unwrap();
-
-            if args.is_empty() {
-                missing_argument(msg, ctx, String::from("AMOUNT")).await;
-                return Ok(())
-            }
-
-            let amount_arg = args.single::<i64>();
-            if amount_arg.is_err() {
-                wrong_argument(msg, ctx, String::from("AMOUNT")).await;
-                return Ok(())
-            }
-
-            let lab = get_lab(doc! { 
-                LabField::UserId.as_str(): &user.to_string(),
-            }).await;
-
-            if lab.is_none() {
-                lab_doesnt_exist(&msg, &ctx).await;
-                return Ok(())
-            }
-
-            if added_item == "money" || added_item == "mo" {
-                let amount = amount_arg.unwrap();
-                lab.unwrap().remove_money(&amount).await;
-                msg.channel_id.say(&ctx.http, &format!("✅ Successfully taken away from {} {} dollars", user.mention(), amount)).await?;
-            }
-            else if added_item == "meth" || added_item == "me" {
-                let amount = amount_arg.unwrap();
-                lab.unwrap().remove_meth(&amount).await;
-                msg.channel_id.say(&ctx.http, &format!("✅ Successfully taken away from {} {} pounds of meth", user.mention(), amount)).await?;
-            }
-            else {
-                wrong_argument(msg, ctx, String::from("AMOUNT")).await;
-                return Ok(())
-            }
+    if !lab_value_change_args_wrapped.is_none() {
+        // Get arguments into separate variables
+        let lab_value_change_args = lab_value_change_args_wrapped.unwrap();
+        let item_type = lab_value_change_args.item_type;
+        let lab = lab_value_change_args.lab;
+        let amount = lab_value_change_args.amount;
+        let user = lab_value_change_args.user;
+        
+        // Check added item type and handle it properly
+        if item_type == "money" || item_type == "mo" {
+            lab.remove_money(&amount).await;
+            msg.channel_id.say(&ctx.http, &format!("✅ Successfully taken away from {} {} dollar(s)", user.mention(), amount)).await?;
+        }
+        else if item_type == "meth" || item_type == "me" {
+            lab.remove_meth(&amount).await;
+            msg.channel_id.say(&ctx.http, &format!("✅ Successfully taken away from {} {} pound(s) of meth", user.mention(), amount)).await?;
+        }
+        else {
+            wrong_argument(msg, ctx, String::from("ITEM TYPE")).await;
+            return Ok(())
         }
     }
     Ok(())
--- src/utils/args.rs
+++ src/utils/args.rs
@@ -1,6 +1,19 @@
+use mongodb::bson::doc;
 use serenity::framework::standard::Args;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
 
+use crate::constants::economy::{LabField, Lab};
 use crate::constants::time::DURATION_TIME;
+use crate::utils::economy::get_lab;
+use crate::utils::errors::{missing_argument, wrong_argument, lab_doesnt_exist};
+
+pub struct LabValueChangeArgs {
+    pub lab: Lab,
+    pub user: UserId,
+    pub amount: i64,
+    pub item_type: String,
+}
 
 // Get a duration from some command arguments
 pub fn get_duration_from_args(args: &mut Args) -> Option<u32> {
@@ -32,4 +45,61 @@ pub fn get_reason_from_args(args: &mut Args) -> String {
         return String::from("reason not provided");
     }
     String::from(args.rest())
+}
+
+// Parse value change commands values and return them (or None if there is an error)
+pub async fn get_lab_value_change_args(ctx: &Context, msg: &Message, mut args: Args) -> Option<LabValueChangeArgs> {
+    // Get the guild member for the author
+    let member = msg.guild_id.unwrap().member(ctx, msg.author.id).await.expect("Error getting member for the author from the guild");
+
+    if member.permissions(ctx).expect("permissions").contains(Permissions::ADMINISTRATOR) {
+        if args.is_empty() {
+            missing_argument(msg, ctx, String::from("TYPE [meth/money]")).await;
+        } else {
+            let added_item_result = args.single::<String>();
+            if added_item_result.is_err() {
+                wrong_argument(msg, ctx, String::from("TYPE [meth/money]")).await;
+                return None
+            }
+            let added_item: String = added_item_result.unwrap();
+ 
+            if args.is_empty() {
+                missing_argument(msg, ctx, String::from("MEMBER")).await;
+                return None
+            }
+
+            // Get user from arguments and throw an error if argument is missing
+            let user_result = args.single::<UserId>();
+            if user_result.is_err() {
+                wrong_argument(msg, ctx, String::from("MEMBER")).await;
+                return None
+            }
+            let user: UserId = user_result.unwrap();
+
+            if args.is_empty() {
+                missing_argument(msg, ctx, String::from("AMOUNT")).await;
+                return None
+            }
+
+            let amount_arg = args.single::<i64>();
+            if amount_arg.is_err() {
+                wrong_argument(msg, ctx, String::from("AMOUNT")).await;
+                return None
+            }
+            let amount = amount_arg.unwrap();
+
+            let lab_arg = get_lab(doc! { 
+                LabField::UserId.as_str(): &user.to_string(),
+            }).await;
+
+            if lab_arg.is_none() {
+                lab_doesnt_exist(&msg, &ctx).await;
+                return None
+            }
+            let lab = lab_arg.unwrap();
+
+            return Some(LabValueChangeArgs { lab: lab, user: user, amount: amount, item_type: added_item })
+        }
+    }
+    None
 }
